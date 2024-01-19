377b9edbc5ecfba781fc83524d786e2a53a0b9b7
JoyRume <joy.rume@student.moringaschool.com>
2024-01-19 21:55:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
+// addEventListeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
+
+
+// get meal list that matches with the ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt})
@@ -33,6 +37,7 @@ function getMealList(){
             html = "Sorry, we didn't find any meal!";
             mealList.classList.add('notFound');
         }
+
         mealList.innerHTML = html;
     });
 }
